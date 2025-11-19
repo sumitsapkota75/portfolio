c1b3985e6d1187dea842d64dda0ff37fd15ac8bd
Sumit <sumitsapkota0@gmail.com>
2025-11-18 23:12:54 -0600

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Sumit Sapkota | Software Engineer",
   description:
     "Full-stack software engineer portfolio – Golang, Python, React, Next.js, cloud-native systems, and scalable microservices.",
-    icons: {
+  icons: {
     icon: "/favicon.png",
     shortcut: "/favicon.png",
     apple: "/favicon.png",
@@ -21,8 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className="bg-neutral-950 text-neutral-50 antialiased text-[16px] md:text-[17px] leading-relaxed">
-  {children}
-</body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
